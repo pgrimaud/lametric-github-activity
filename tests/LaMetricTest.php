bf28ee5bad2fe9b36c719bb03eadeb79ef621210
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-06-23 17:02:26 +0200

    Fix tests

--- tests/LaMetricTest.php
+++ tests/LaMetricTest.php
@@ -4,36 +4,11 @@ declare(strict_types=1);
 
 namespace LaMetric\tests;
 
-use GuzzleHttp\Client;
-use LaMetric\Api;
 use LaMetric\Response;
-use LaMetric\Validator;
 use PHPUnit\Framework\TestCase;
 
 class LaMetricTest extends TestCase
 {
-    public function testCompleteProcess(): void
-    {
-        $httpGetData = [];
-
-        $validator = new Validator($httpGetData);
-        $result    = $validator->check([]);
-
-        $this->assertEquals(true, $result);
-
-        $api    = new Api(new Client(), []);
-        $frames = $api->fetchData($validator->getData());
-
-        $this->assertIsArray($frames->getFrames());
-
-        $response = new Response();
-        $json     = $response->printData($frames);
-
-        $jsonFixtures = file_get_contents(__DIR__ . '/fixtures/response.json');
-
-        $this->assertSame($jsonFixtures, $json);
-    }
-
     public function testInvalidProcess(): void
     {
         $response = new Response();
